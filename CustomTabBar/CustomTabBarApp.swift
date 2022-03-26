9d8056d936387b33eedb4662dbe2af3e678075a5
袁志健 <jswxyzj@qq.com>
2022-03-26 12:56:16 +0800

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by 袁志健 on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
